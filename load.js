726e2c87808c78fa1e5c55578096ba98ff4760c0
ramadityas <ramaditya.id@gmail.com>
2016-10-21 14:52:54 +0700

    pertama

---
+++ load.js
@@ -0,0 +1,19 @@
+var delay = 3000;
+setTimeout(function() 
+           
+    {  
+        
+        $( "#pa_overlay" ).fadeOut( "slow" );
+     $( "body" ).css( "background-color", "white" );
+         $('#pa_container').fadeIn();
+     
+
+    },
+    delay
+) ;
+
+
+  
+
+
+ 
